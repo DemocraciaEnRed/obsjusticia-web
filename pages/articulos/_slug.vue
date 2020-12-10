39645a3975e6684a91dc951c8b429a062bc4f774
gaabicarp <gaabicarp@gmail.com>
2020-12-10 11:28:35 -0300

    Ajustes articulos

--- pages/articulos/_slug.vue
+++ pages/articulos/_slug.vue
@@ -13,60 +13,64 @@
     </div>
     <div class="columns">
       <div class="column is-three-quarters">
-        <div class="my-6">
-          <p class="is-size-3 has-text-weight-bold">
-            {{ post.introduccion }}
-          </p>
-        </div>
-        <div v-for="parrafo in post.parafos" :key="`parrafo-${parrafo.titulo}`" class="is-size-3 my-6">
-          <p class="is-size-2 has-text-weight-bold my-3">
-            {{ parrafo.titulo }}
-          </p>
-          <p>
-            {{ parrafo.contenido }}
-          </p>
+        <div class="container">
+          <div class="my-6">
+            <p class="is-size-4 has-text-weight-bold">
+              {{ post.introduccion }}
+            </p>
+          </div>
+          <div v-for="parrafo in post.parafos" :key="`parrafo-${parrafo.titulo}`" class="is-size-4 my-6">
+            <p class="is-size-3 has-text-weight-bold my-3">
+              {{ parrafo.titulo }}
+            </p>
+            <p>
+              {{ parrafo.contenido }}
+            </p>
+          </div>
         </div>
       </div>
       <div class="column mb-6">
-        <div class="box has-background-white py-6 mt-6">
-          <div class="has-text-centered">
-            <p class="is-size-1">
-              TEMAS
-            </p>
-            <div class="mt-4">
-              <p class="is-size-4">
-                #ACIJ #JUDICIAL #MAFALDA #REDESSOCIALES
+        <div class="container">
+          <div class="box has-background-white py-6 mt-6 mx-6">
+            <div class="has-text-centered">
+              <p class="is-size-1">
+                TEMAS
               </p>
+              <div class="mt-4">
+                <p class="is-size-4">
+                  #ACIJ #JUDICIAL #MAFALDA #REDESSOCIALES
+                </p>
+              </div>
             </div>
-          </div>
-          <div class="columns has-text-centered is-size-2 mt-5">
-            <div class="column">
-              <a href="https://www.facebook.com/ACIJ.ORG">
-                <span class="icon">
-                  <i class="fab fa-facebook-f" />
-                </span>
-              </a>
-            </div>
-            <div class="column">
-              <a href="https://twitter.com/ACIJargentina">
-                <span class="icon">
-                  <i class="fab fa-twitter" />
-                </span>
-              </a>
-            </div>
-            <div class="column">
-              <a href="https://www.linkedin.com/company/asociacióncivilporlaigualdadylajusticia/">
-                <span class="icon">
-                  <i class="fab fa-linkedin" />
-                </span>
-              </a>
-            </div>
-            <div class="column">
-              <a href="https://www.youtube.com/user/CanalACIJ">
-                <span class="icon">
-                  <i class="fab fa-youtube" />
-                </span>
-              </a>
+            <div class="columns has-text-centered is-size-2 mt-5">
+              <div class="column">
+                <a href="https://www.facebook.com/ACIJ.ORG">
+                  <span class="icon">
+                    <i class="fab fa-facebook-f" />
+                  </span>
+                </a>
+              </div>
+              <div class="column">
+                <a href="https://twitter.com/ACIJargentina">
+                  <span class="icon">
+                    <i class="fab fa-twitter" />
+                  </span>
+                </a>
+              </div>
+              <div class="column">
+                <a href="https://www.linkedin.com/company/asociacióncivilporlaigualdadylajusticia/">
+                  <span class="icon">
+                    <i class="fab fa-linkedin" />
+                  </span>
+                </a>
+              </div>
+              <div class="column">
+                <a href="https://www.youtube.com/user/CanalACIJ">
+                  <span class="icon">
+                    <i class="fab fa-youtube" />
+                  </span>
+                </a>
+              </div>
             </div>
           </div>
         </div>
