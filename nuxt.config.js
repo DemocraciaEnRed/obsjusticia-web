f6f7553c7f7b0b88e0a60a0a12bff9388d0009ac
Guillermo Croppi <guillermocroppi@gmail.com>
2020-12-23 11:56:20 -0300

    Added robots no-index and target static (for now)

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,13 @@
 export default {
+  target: 'static',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'ACIJ - Observatorio de Justicia',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { hid: 'robots', name: 'robots', content: 'noindex' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
