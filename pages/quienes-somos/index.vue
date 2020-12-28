018bbfe31385ded12ab425465aec6a63d03e407a
gaabicarp <gaabicarp@gmail.com>
2020-12-28 10:02:36 -0300

    Pagina quiees somos

---
+++ pages/quienes-somos/index.vue
@@ -0,0 +1,116 @@
+<template>
+  <section class="section">
+    <div class="section columns">
+      <div class="column is-half is-flex">
+        <div class="column is-two-fifths is-flex is-justify-content-center is-align-items-center">
+          <img src="~/assets/images/logo_justa@3x.png">
+        </div>
+        <div class="column">
+          <p class="is-size-2">plataforma por una justicia <br>transparente y abierta</p>
+        </div>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column ml-6">
+        <p class="is-size-2 is-raleway">¿Que motiva <span class="has-text-weight-bold">JusTA?</span></p>
+        <div class="line" />
+        <p class="is-size-4 is-chivo mb-4 has-text-weight-light has-text-black">
+          Observamos que mientras hay un consenso general de que la ciudadanía necesita una justicia más eficaz, transparente e independiente pero que, a la vez, hace décadas que no muestra cambios sustantivos en ninguna de esas tres facetas. El estado actual del sistema judicial es una de las causas de la deteriorada salud de la democracia, algo que es percibido por una buena parte de la sociedad civil en todas las encuestas de opinión que se realizan sobre la materia.
+        </p>
+        <p class="is-size-4 is-chivo mb-4 has-text-weight-light has-text-black">
+          Tenemos una justicia con enormes defectos, con problemas estructurales de funcionamiento que se arrastran desde hace décadas, pero, también, de independencia, opacidad y falta de rendición de cuentas.<br>Quienes utilizamos cotidianamente la justicia (profesionales del derecho, integrantes del propio sistema de justicia, organizaciones de la sociedad civil), sumados a la academia, el periodismo y a otros analistas e investigadores, tenemos mucho para decir, promover y sumar en este objetivo. Algo similar sucede con tantos integrantes del sistema judicial que sienten que debe haber cambios en las instituciones a las que pertenecen para que se sientan mejor representados por ellas.
+        </p>
+        <p class="is-size-4 is-chivo mb-4 has-text-weight-light has-text-black">
+          Proponemos JusTA como una iniciativa de intercambio de información, propuestas, diálogos, reclamos, interacciones con los actores del sistema judicial y cualquier otro tipo de vinculación virtuosa que surja de hacernos escuchar. JusTA promueve una mayor apertura y rendición de cuentas hacia la ciudadanía de los órganos del sistema judicial nacional (particularmente la Corte Suprema y el Consejo de la Magistratura, pero, también, el Ministerio Público Fiscal y de la Defensa).
+        </p>
+      </div>
+      <div class="column">
+        <div class="is-flex is-justify-content-center is-align-items-center">
+          <div style="width: 50%;" class="bg-violet">
+            <img src="~/assets/images/palacio-justicia.jpg" class="image">
+          </div>
+        </div>
+        <div class="columns mt-4">
+          <div class="column">
+            <div class="is-flex is-justify-content-center is-align-items-center">
+              <img src="~/assets/images/marcha.jpg" class="image">
+            </div>
+          </div>
+          <div class="column">
+            <div class="is-flex is-justify-content-center is-align-items-center">
+              <img src="~/assets/images/martillo.jpg" class="image">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="columns mb-6">
+      <div class="column is-two-fifths">
+        <img src="~/assets/images/ateneo.jpg" class="image mb-4" style="width: 60%;">
+        <img src="~/assets/images/catedral.jpg" class="image" style="width: 40%;">
+      </div>
+      <div class="column">
+        <p class="is-size-2 is-raleway">¿Que contenidos <span class="has-text-weight-bold">publica JusTA?</span></p>
+        <div class="line" />
+        <p class="is-size-4 is-chivo mb-4 has-text-weight-light has-text-black">
+          JusTA es una plataforma digital desde la cual se:
+        </p>
+        <ul class="ml-6 mb-5" style="list-style: disc;">
+          <li class="is-size-4 is-chivo mb-2 has-text-weight-light has-text-black">
+            Identifica y resume información relevante sobre el sistema judicial argentino que hoy es de difícil acceso, se solicita otra que no se encuentra disponible y se la visibiliza.
+          </li>
+          <li class="is-size-4 is-chivo mb-2 has-text-weight-light has-text-black">
+            Visualizan y publican datos sobre el sistema de justicia de forma accesible.
+          </li>
+          <li class="is-size-4 is-chivo mb-2 has-text-weight-light has-text-black">
+            Examinan distintos aspectos de las instituciones que lo integran, cuya necesaria mejora es vital, y se realizan propuestas.
+          </li>
+          <li class="is-size-4 is-chivo mb-2 has-text-weight-light has-text-black">
+            Se realizan intercambios entre distintos actores vinculados al sistema judicial y se promueven acciones colaborativas.
+          </li>
+          <li class="is-size-4 is-chivo mb-2 has-text-weight-light has-text-black">
+            Se publican opiniones de expertos sobre temas estructurales vinculados al funcionamiento del sistema judicial y su independencia.
+          </li>
+        </ul>
+        <p class="is-size-4 is-chivo mb-4 has-text-weight-light has-text-black">
+          Entre los temas prioritarios de la plataforma se incluyen:
+        </p>
+        <ul class="ml-6" style="list-style: disc;">
+          <li class="is-size-4 is-chivo mb-2 has-text-weight-light has-text-black">
+            La transparencia activa y pasiva de la Corte Suprema, Consejo de la Magistratura, Ministerio Público Fiscal y Ministerio Público de la Defensa;
+          </li>
+          <li class="is-size-4 is-chivo mb-2 has-text-weight-light has-text-black">
+            La forma de selección de sus autoridades y personal;
+          </li>
+          <li class="is-size-4 is-chivo mb-2 has-text-weight-light has-text-black">
+            Los procesos disciplinarios y acusatorios contra sus integrantes;
+          </li>
+          <li class="is-size-4 is-chivo mb-2 has-text-weight-light has-text-black">
+            Los tiempos de los procesos que generan la extendida sensación de ausencia de justicia y los problemas de independencia especialmente relacionados a sus vínculos con los poderes políticos, actores económicos, servicios de inteligencia, entre otros.
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+<style lang="scss" scoped>
+  .title-container{
+    margin-left: 5em;
+  }
+
+  .section{
+      background-color: white;
+  }
+  .line {
+    display:inline-block;
+    width: 367px;
+    height: 1px;
+    margin: 1em 0em 2em 0em;
+    background-color: #000000;
+  }
+
+  .image{
+      width: 100%;
+  }
+
+</style>
