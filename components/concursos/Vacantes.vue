959bf68f7c779703f2f56b9228e897effe7b6382
Guillermo Croppi <guillermocroppi@gmail.com>
2021-08-24 22:40:59 -0300

    Migrating Google Sheets graphs from v3 to v4

--- components/concursos/Vacantes.vue
+++ components/concursos/Vacantes.vue
@@ -198,7 +198,7 @@ export default {
   },
   async fetch () {
     const data = await fetch(
-      'https://spreadsheets.google.com/feeds/list/1BA3bVbUA8gOYGmNCnH4vvlBohvTKZfXi6j2lh9BpZCA/1/public/full?alt=json'
+      `https://sheets.googleapis.com/v4/spreadsheets/1BA3bVbUA8gOYGmNCnH4vvlBohvTKZfXi6j2lh9BpZCA/values/Sheet1?key=${this.$config.googleSheetApiKey}`
     ).then(res => res.json())
     this.data = this.extractData(data)
   },
@@ -215,11 +215,13 @@ export default {
     extractData (data) {
       // eslint-disable-next-line prefer-const
       let output = []
-      data.feed.entry.forEach((entry) => {
+      const theKeys = this.keys
+      const theValues = data.values.slice(1)
+      theValues.forEach((entry) => {
         // eslint-disable-next-line prefer-const
-        let marker = {}
-        this.keys.forEach((key) => {
-          marker[key] = entry[`gsx$${key}`].$t !== '' ? entry[`gsx$${key}`].$t : null
+        const marker = {}
+        theKeys.forEach((k, i) => {
+          marker[k] = entry[i] !== '' ? entry[i] : null
         })
         output.push(marker)
       })
