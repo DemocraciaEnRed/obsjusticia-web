6ac669b416f74627c714f9855b0a0d70d6e3a993
DevHormiga <devhormiga@gmail.com>
2022-04-08 10:18:12 -0300

    link a la font OSWALD

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,7 @@ export default {
       { href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css', rel: 'stylesheet', integrity: 'sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==', crossorigin: 'anonymous' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Chivo:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap' },
       { rel: 'text/javascript', href: '/node_modules/bulma-accordion/dist/js/bulma-accordion.min.js'}
 
     ]
