786a1e7ebd0367cb5dc1a1f82b2d69a54db8c9fb
Nato Szmedra <natoszmedra@gmail.com>
2022-04-28 21:38:12 -0300

    feat: process attorneys image + change both texts

--- components/concursos/fiscales/Proceso.vue
+++ components/concursos/fiscales/Proceso.vue
@@ -6,12 +6,9 @@
           ¿Cómo se seleccionan fiscales y fiscalías en nuestro país?
         </h3>
         <h1 class="subtitle is-4 my-6 line-height-150">
-          El proceso de selección consiste en una serie de pasos en los que intervienen distintos órganos: el Consejo de la Magistratura, el Poder Ejecutivo y el Senado.
+          La siguiente infografía muestra los pasos que se deben seguir para llegar a una designación:
         </h1>
-        <h1 class="subtitle is-4 is-700 is-spaced mb-6 line-height-150">
-          Estas son las etapas más importantes de este proceso:
-        </h1>
-        <img src="~/assets/img/concursos/proceso-desktop.svg" class="image is-centered is-fullwidth is-hidden-mobile my-6" alt="">
+        <img src="~/assets/img/concursos/fiscales-desktop.svg" class="image is-centered is-fullwidth is-hidden-mobile my-6" alt="">
         <img src="~/assets/img/concursos/proceso-mobile.svg" class="image is-centered is-fullwidth is-hidden-tablet my-6" alt="">
       </div>
     </div>
